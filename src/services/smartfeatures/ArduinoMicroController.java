e396fd660300f1d2499fd9cc2aecedc31a78b2cd
Jcarlos2001 <152795548+Juanca192001@users.noreply.github.com>
2024-12-12 15:44:53 +0100

    update interface Arduino

--- src/services/smartfeatures/ArduinoMicroController.java
+++ src/services/smartfeatures/ArduinoMicroController.java
@@ -1,4 +1,13 @@
 package services.smartfeatures;
 
+import java.net.ConnectException;
+
 public interface ArduinoMicroController {
+    void setBTconnection() throws ConnectException;
+
+    void startDriving() throws PMVPhisicalException, ConnectException, ProceduralException;
+
+    void stopDriving() throws PMVPhisicalException, ConnectException, ProceduralException;
+
+    void undoBTconnection();
 }
