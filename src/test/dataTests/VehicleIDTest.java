bbf5a68e82bd90c7f7001a7585fa88d7ac8dbc6e
Jcarlos2001 <152795548+Juanca192001@users.noreply.github.com>
2024-12-19 09:05:00 +0100

    VehicleIDTests advancment

--- src/test/dataTests/VehicleIDTest.java
+++ src/test/dataTests/VehicleIDTest.java
@@ -1,5 +1,12 @@
 package test.dataTests;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import data.VehicleID;
 public class VehicleIDTest {
+    //Aqui començent els testos del constructor
+    @Test
+    void VehicleIDConstructorTest(){
+        assertThrows(IllegalArgumentException.class, () -> new VehicleID(""));
+    }
+
 }
