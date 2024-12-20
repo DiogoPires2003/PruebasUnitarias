b680187660ab509761435efd61abf9d9673d7b72
Jcarlos2001 <152795548+Juanca192001@users.noreply.github.com>
2024-12-20 19:54:15 +0100

    setup test

--- src/test/micromobilityTests/JourneyRealizeHandlerTest.java
+++ src/test/micromobilityTests/JourneyRealizeHandlerTest.java
@@ -1,5 +1,30 @@
 package test.micromobilityTests;
+import micromobility.JourneyRealizeHandler;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import services.ImplementsServer;
+import services.Server;
+import services.smartfeatures.*;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 public class JourneyRealizeHandlerTest {
+
+    JourneyRealizeHandler journeyRH;
+    Server server;
+    QRDecoder qrDecoder;
+    ArduinoMicroController arduinoMicroController;
+    UnbondedBTSignal btSignal;
+
+    @BeforeEach
+    void setup(){
+        this.server = new ImplementsServer();
+        this.qrDecoder = new ImplementsQRDecoder();
+        this.arduinoMicroController = new ImplementsArduinoMicroController();
+        this.btSignal = new ImplementsUnbondedBTSignal();
+        this.journeyRH = new JourneyRealizeHandler(server,qrDecoder,arduinoMicroController,btSignal);
+    }
+
+
+
 }
